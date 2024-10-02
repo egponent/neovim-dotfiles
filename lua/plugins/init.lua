044f7f855c30246f8a657f9eb43825b4022cc9aa
Luka Vasko <egponent@gmail.com>
2024-10-02 19:57:07 -0400

    added neorg and latex support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,9 +3,7 @@ return {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
-  },
-
-  -- These are some examples, uncomment them if you want to see them work!
+  },  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -16,6 +14,25 @@ return {
     "xiyaowong/transparent.nvim",
     lazy = false
   },
+  {
+  "lervag/vimtex",
+  ft = "tex",
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_view_general_viewer = "okular"
+    vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+    vim.g.tex_flavor = "latex"
+    vim.g.vimtex_quickfix_mode=0
+    vim.g.tex_conceal= "abdmg"
+  end
+  },
+  {
+    "nvim-neorg/neorg",
+    ft = "norg",
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+}
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
