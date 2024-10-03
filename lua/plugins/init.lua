13fc1ebf2dc386a8e5f3618afc810476d5b705a4
Luka Vasko <egponent@gmail.com>
2024-10-02 21:10:20 -0400

    removed latex support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,24 +15,10 @@ return {
     lazy = false
   },
   {
-  "lervag/vimtex",
-  ft = "tex",
-  -- tag = "v2.15", -- uncomment to pin to a specific release
-  init = function()
-    -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_general_viewer = "okular"
-    vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
-    vim.g.tex_flavor = "latex"
-    vim.g.vimtex_quickfix_mode=0
-    vim.g.tex_conceal= "abdmg"
-  end
-  },
-  {
     "nvim-neorg/neorg",
     ft = "norg",
     version = "*", -- Pin Neorg to the latest stable release
-    config = true,
-}
+  }
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
