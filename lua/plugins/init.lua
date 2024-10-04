3203e8f4b8610879066b3014a066d5661e003d17
Luka Vasko <egponent@gmail.com>
2024-10-04 16:05:04 -0400

    plugin config change

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
-  },  -- These are some examples, uncomment them if you want to see them work!
+  },
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -12,21 +12,51 @@ return {
   },
   {
     "xiyaowong/transparent.nvim",
-    lazy = false
+    lazy = false,
   },
+  {
+  "lervag/vimtex",
+  ft = "tex",
+  init = function()
+    vim.g.vimtex_view_general_viewer = "okular"
+    vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+  end,
+},
   {
     "nvim-neorg/neorg",
     ft = "norg",
     version = "*", -- Pin Neorg to the latest stable release
-  }
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    config = function()
+        require("neorg").setup {
+          load = {
+            ["core.defaults"] = {},
+            ["core.concealer"] = {},
+            ["core.dirman"] = {
+              config = {
+                workspaces = {
+                  notes = "~/Documents/Notes/",
+                },
+                default_workspace = "notes",
+              },
+            },
+          },
+        }
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css", "norg", "go"
+      },
+      highlight = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
